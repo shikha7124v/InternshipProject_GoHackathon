e048be17a50767b74323cc897312194046f6e7ca
shikha7124 <shikhaverma7124@gmail.com>
2023-02-26 19:51:44 +0530

    Changes in the main page

--- main.js
+++ main.js
@@ -4,15 +4,8 @@ let nCount = selector => {
         .animate({
           Counter: $(this).text()
         }, {
-          // A string or number determining how long the animation will run.
           duration: 4000,
-          // A string indicating which easing function to use for the transition.
           easing: "swing",
-          /**
-           * A function to be called for each animated property of each animated element. 
-           * This function provides an opportunity to
-           *  modify the Tween object to change the value of the property before it is set.
-           */
           step: function (value) {
             $(this).text(Math.ceil(value));
           }
@@ -22,7 +15,6 @@ let nCount = selector => {
   
   let a = 0;
   $(window).scroll(function () {
-    // The .offset() method allows us to retrieve the current position of an element  relative to the document
     let oTop = $(".numbers").offset().top - window.innerHeight;
     if (a == 0 && $(window).scrollTop() >= oTop) {
       a++;
@@ -30,18 +22,8 @@ let nCount = selector => {
     }
   });
   
-  
-  
-  /**
-   *
-   *  sticky navigation
-   *
-   */
-  
   let navbar = $(".navbar");
-  
   $(window).scroll(function () {
-    // get the complete hight of window
     let oTop = $(".section-2").offset().top - window.innerHeight;
     if ($(window).scrollTop() > oTop) {
       navbar.addClass("sticky");
