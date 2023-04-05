193a49073df62d8f349391bffc8e450816d51106
Deeksha Chaturvedi <deeksha@hocalwire.com>
2023-04-05 11:41:54 +0530

    mentorship page

---
+++ js/mentorship.js
@@ -0,0 +1,24 @@
+
+// let skills = ["Business", "Backend", "Leadership", "Engineering", "Product Management", "Machine Learning", "Design", "Startup", "JavaScript", "Marketing", "Python", "Entrepreneur", "UX Design", "Product", "Data Science", "Agile", "Sales", "User Research", "Web Development", "DevOps", "Product Marketing", "SEO", "Branding", "Growth", "CEO", "Marketing", "Strategy", "AWS", "React", "Data Science", "SEO", "SaaS", "Interviewing", "Resumé"]
+// new typed('#mentorship-typed-headline', {
+//     strings: skills.map(value => ({
+//         value,
+//         sort: Math.random()
+//     })).sort((a, b) => a.sort - b.sort).map(({
+//         value
+//     }) => value),
+//     speed: 80,
+//     breakLines: false,
+//     autoStart: true,
+//     lifelike: true,
+//     nextStringDelay: [200, 1500],
+//     startDelete: true,
+//     loop: true
+// });
+
+// amplitude.getInstance().logEvent("Visit homepage");
+
+// function scroll() {
+//     document.getElementById( 'image-block1' ).scrollIntoView();
+// };
+// scroll();
