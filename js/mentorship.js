6d493b91a1fe478745c5e7ba7acdd6f8ebbe9237
Deeksha <rapido@Rapidos-MacBook-Pro.local>
2023-04-19 09:34:52 +0530

    mentorship 19 april

--- js/mentorship.js
+++ js/mentorship.js
@@ -1,24 +1,78 @@
+// Typing Effect Box Start
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 3000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-// let skills = ["Business", "Backend", "Leadership", "Engineering", "Product Management", "Machine Learning", "Design", "Startup", "JavaScript", "Marketing", "Python", "Entrepreneur", "UX Design", "Product", "Data Science", "Agile", "Sales", "User Research", "Web Development", "DevOps", "Product Marketing", "SEO", "Branding", "Growth", "CEO", "Marketing", "Strategy", "AWS", "React", "Data Science", "SEO", "SaaS", "Interviewing", "Resumé"]
-// new typed('#mentorship-typed-headline', {
-//     strings: skills.map(value => ({
-//         value,
-//         sort: Math.random()
-//     })).sort((a, b) => a.sort - b.sort).map(({
-//         value
-//     }) => value),
-//     speed: 80,
-//     breakLines: false,
-//     autoStart: true,
-//     lifelike: true,
-//     nextStringDelay: [200, 1500],
-//     startDelete: true,
-//     loop: true
-// });
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-// amplitude.getInstance().logEvent("Visit homepage");
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-// function scroll() {
-//     document.getElementById( 'image-block1' ).scrollIntoView();
-// };
-// scroll();
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
+// Typing Effect Box End
+
+// read More Button popup
+$ = function(id) {
+    return document.getElementById(id);
+  }
+  
+  var show = function(id) {
+      $(id).style.display ='block';
+  }
+  var hide = function(id) {
+      $(id).style.display ='none';
+  }
+
+
+  $("body").on('click', '.popular-mentorships-nav-item', function () {
+    var showList = $(this).active(".popular-mentorships-tab-pane").find(".show-list");
+    var hideList = $(this).active(".popular-mentorships-tab-pane").find(".hide-list");
+    if (showList.hasClass("popular-mentorships-nav-item")) {
+        showList.addClass("active");
+        hideList.removeClass("hide");
+    } else {
+        showList.removeClass("hide");
+        hideList.addClass("active");
+    }
+});
